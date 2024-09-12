908d983b575ac25b5774ef988690192e63388e93
DevAkashKhandagale <akashakhandagale@gmail.com>
2024-09-12 13:11:29 +0530

    try catch added for date formatting

--- app/src/main/java/com/devakashk/mynewsapp/utils/string_formatters.kt
+++ app/src/main/java/com/devakashk/mynewsapp/utils/string_formatters.kt
@@ -1,4 +1,5 @@
 package com.devakashk.mynewsapp.utils
+import android.util.Log
 import com.devakashk.mynewsapp.models.NewsItem
 import com.google.gson.Gson
 import java.text.SimpleDateFormat
@@ -6,11 +7,17 @@ import java.util.Locale
 
 
 fun String.formattedDate(): String {
+
     val inputFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.getDefault())
     val outputFormat = SimpleDateFormat("EEE, dd MMM yyyy", Locale.getDefault())
 
-    val date = inputFormat.parse(this) ?: return ""
-    return outputFormat.format(date)
+    try{
+        val date = inputFormat.parse(this) ?: return ""
+        return outputFormat.format(date)
+    }catch (e:Exception){
+        Log.d("STRING_FORMATTERS", "formattedDate: $e")
+    }
+    return this
 }
 
 fun String.toNewsItem(): NewsItem {
